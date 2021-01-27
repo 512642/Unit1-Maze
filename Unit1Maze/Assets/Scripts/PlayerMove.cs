3addaffab76faf67fff10f8f81f0eba48db409db
512642 <72399788+512642@users.noreply.github.com>
2021-01-27 12:33:31 +0000

    everything works

--- Unit1Maze/Assets/Scripts/PlayerMove.cs
+++ Unit1Maze/Assets/Scripts/PlayerMove.cs
@@ -2,25 +2,19 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    float saveSpeed = 3.0f;
-    float crouchSpeed = 1.0f;
+
     public float speed = 3.0f;
-    bool collided = false;
     int LifeCounter = 3; // this can be an int as you are only using whole numbers
     //public bool crouched = false;
     public Rigidbody2D rb;
     public Transform spawn;
     bool crouching = false;
-    private Animator anim;
-    float xv = 3;
-    float yv = 3;
 
 
 
     void start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
      // Update is called once per frame
@@ -28,7 +22,6 @@ public class PlayerMove : MonoBehaviour
     {
         EndGame();
         SlowWalk();
-        NormalWalk();
         Walking();
 
     }
@@ -41,7 +34,6 @@ public class PlayerMove : MonoBehaviour
 
         if (col.collider.tag == "Walls")
         {
-            collided = true;
             transform.position = spawn.position;
             // If your intention is to decrease the lifecounter when the player hits a wall, add your code here.       
             LifeCounter--;
@@ -51,33 +43,32 @@ public class PlayerMove : MonoBehaviour
 
         }
     }
-
-    void NormalWalk()
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            print("uncrouching");
-            crouching = false;
-            if (crouching == false)
-            {
-                yv = 0;
-                xv = 0;
-                rb.velocity = new Vector2(xv, yv);
-            }
-
-        }
-    }
     void SlowWalk()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey("up"))
         {
-            print("crouching");
             crouching = true;
             if (crouching == true)
             {
-                yv = -+1;
-                xv = -+1;
+                float yv = 0;
+                float xv = 0;
+                if (Input.GetKey("w"))
+                {
+                    yv = 1;
+                }
+                if (Input.GetKey("a"))
+                {
+                    xv = -1;
 
+                }
+                if (Input.GetKey("s"))
+                {
+                    yv = -1;
+                }
+                if (Input.GetKey("d"))
+                {
+                    xv = 1;
+                }
                 rb.velocity = new Vector2(xv, yv);
             }
         }
@@ -86,32 +77,32 @@ public class PlayerMove : MonoBehaviour
         void Walking()
         {
 
-            float xv = 0;
-            float yv = 0;
+             float xv = 0;
+             float yv = 0;
 
-        if (Input.GetKey("w"))
-            {
-                yv = 3;
-            }
-            if (Input.GetKey("a"))
-            {
-                xv = -3;
+            if (Input.GetKey("w"))
+             {
+                 yv = 3;
+             }
+             if (Input.GetKey("a"))
+             {
+                 xv = -3;
 
-            }
-            if (Input.GetKey("s"))
-            {
-                yv = -3;
-            }
-            if (Input.GetKey("d"))
-            {
-                xv = 3;
-            }
+             }
+             if (Input.GetKey("s"))
+             {
+                 yv = -3;
+             }
+             if (Input.GetKey("d"))
+             {
+                 xv = 3;
+             }
+                // set the x and y velocity
+             rb.velocity = new Vector2(xv, yv);
+        SlowWalk();
 
 
-            // set the x and y velocity
-            rb.velocity = new Vector2(xv, yv);
-
-        }
+    }
 
         void EndGame()
         {
